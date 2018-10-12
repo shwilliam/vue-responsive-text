5c4a87b75a20f0249d8ffd382e4ff5a706aa55c5
William L <w-lindvall@outlook.com>
2018-10-12 15:08:08 +1100

    sfc example

---
+++ examples/example-1.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div :style="{ width: textWrapperWidth + '%', backgroundColor: 'lightBlue', padding: '50px 0' }">
+      <VueResponsiveText>
+        {{ inputText }}
+      </VueResponsiveText>
+    </div>
+    
+    <input type="text" v-model="inputText">
+    <input type="range" min="20" max="100" 
+      v-model="textWrapperWidth"
+    />
+  </div>
+</template>
+
+<script>
+import VueResponsiveText from "../src/vue-responsive-text.vue";
+
+export default {
+  name: "Example1",
+  data() {
+    return {
+      textWrapperWidth: 50,
+      inputText: "this text will scale"
+    };
+  },
+  components: {
+    VueResponsiveText
+  }
+};
+</script>
